7a9709be3bd7a14aa4acf3f4b150ff823d3a9062
Joan Serra <joan.serra@weareadaptive.com>
2018-10-24 20:32:03 +0200

    Remove embarrassment

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -4,7 +4,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div id="content">
-        <h5>Time to <a href="https://facebook.github.io/react/">React</a>.</h5>
+        (Seriously, the best)
       </div>
     );
   }
